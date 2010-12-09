e017a6363ca8d2b55e80e4628721a7ae6e92f0e7
Justin DeWind <dewind@atomicobject.com>
2010-12-09 06:47:40 -0500

    somehow put the implementation in the header and got away with it

--- Source/ObjectionFunctions.m
+++ Source/ObjectionFunctions.m
@@ -1,5 +1,38 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-extern Class ObjectionFindClassForProperty(objc_property_t property);
-extern objc_property_t ObjectionGetProperty(Class klass, NSString *propertyName);
+static NSString *const ObjectionException = @"ObjectionException";
+
+Class ObjectionFindClassForProperty(objc_property_t property) {
+  NSString *attributes = [NSString stringWithCString: property_getAttributes(property) encoding: NSASCIIStringEncoding];  
+  NSString *propertyName = [NSString stringWithCString:property_getName(property) encoding:NSASCIIStringEncoding];
+  
+  NSRange startRange = [attributes rangeOfString:@"T@\""];
+  if (startRange.location == NSNotFound) {
+    @throw [NSException exceptionWithName:ObjectionException reason:[NSString stringWithFormat:@"Unable to determine class type for property declaration: '%@'", propertyName] userInfo:nil];
+  }
+  
+  NSString *startOfClassName = [attributes substringFromIndex:startRange.length];
+  NSRange endRange = [startOfClassName rangeOfString:@"\""];
+  
+  if (endRange.location == NSNotFound) {
+    @throw [NSException exceptionWithName:ObjectionException reason:[NSString stringWithFormat:@"Unable to determine class type for property declaration: '%@'", propertyName] userInfo:nil];        
+  }
+  
+  NSString *className = [startOfClassName substringToIndex:endRange.location];
+  Class theClass = NSClassFromString(className);
+  
+  if(!theClass) {
+    @throw [NSException exceptionWithName:ObjectionException reason:[NSString stringWithFormat:@"Unable get class for name '%@' for property '%@'", className, propertyName] userInfo:nil];            
+  }
+  
+  return theClass;      
+}
+
+objc_property_t ObjectionGetProperty(Class klass, NSString *propertyName) {
+  objc_property_t property = class_getProperty(klass, (const char *)[propertyName UTF8String]);
+  if (property == NULL) {
+    @throw [NSException exceptionWithName:ObjectionException reason:[NSString stringWithFormat:@"Unable to find property declaration: '%@'", propertyName] userInfo:nil];
+  }
+  return property;
+}
