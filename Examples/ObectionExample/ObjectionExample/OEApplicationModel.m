c18cdf3ffd7b857f935d39e3dccdf0e9b3cf3b80
Justin DeWind <dewind@atomicobject.com>
2011-06-10 12:07:12 -0400

    Removing header comment

--- Examples/ObectionExample/ObjectionExample/OEApplicationModel.m
+++ Examples/ObectionExample/ObjectionExample/OEApplicationModel.m
@@ -1,11 +1,3 @@
-//
-//  OEApplicationModel.m
-//  ObjectionExample
-//
-//  Created by Sean Fisk on 6/3/11.
-//  Copyright 2011 Atomic Object. All rights reserved.
-//
-
 #import "OEApplicationModel.h"
 #import "OETableViewController.h"
 
