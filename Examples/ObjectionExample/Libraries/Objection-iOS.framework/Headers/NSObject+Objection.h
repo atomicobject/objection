23df88d62686e18514c6f12250141c8a5108fd00
Justin DeWind <dewind@atomicobject.com>
2011-06-17 10:38:57 -0400

    Upgraded objection framework in example

--- Examples/ObjectionExample/Libraries/Objection-iOS.framework/Headers/NSObject+Objection.h
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface NSObject(Objection)
-- (void)awakeFromObjection;
-@end
--- Examples/ObjectionExample/Libraries/Objection-iOS.framework/Headers/ObjectionBindingEntry.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "ObjectionEntry.h"
-
-@interface ObjectionBindingEntry : ObjectionEntry {
-	id _instance;
-}
-
-- (id)initWithObject:(id)theObject;
-@end
