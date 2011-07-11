6778334d69002ddebc793296f32e543d9772d7e9
Justin DeWind <dewind@atomicobject.com>
2011-07-10 20:29:48 -0400

    Removing unused libraries in example

--- Examples/ObjectionExample/Frameworks/Test/Cedar-iPhone.framework/Headers/CDRColorizedReporter.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "CDRDefaultReporter.h"
-
-@interface CDRColorizedReporter : CDRDefaultReporter
-
-@end
--- Examples/ObjectionExample/Frameworks/Test/Cedar-iPhone.framework/Versions/Current/Headers/CDRFunctions.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@protocol CDRExampleReporter;
-
-int runSpecsWithCustomExampleReporter(NSArray *specClasses, id<CDRExampleReporter> runner);
-int runAllSpecs();
-int runAllSpecsWithCustomExampleReporter(id<CDRExampleReporter> runner);
