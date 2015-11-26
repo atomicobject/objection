6b9d539131d249f33e875824692b34e9909b6205
Paul Zabelin <pzabelin+git@gmail.com>
2015-11-25 21:13:36 -0800

    refactor spec to reuse existing fixtures

--- Specs/InjectImplementsProtocolSpecs.m
+++ Specs/InjectImplementsProtocolSpecs.m
@@ -2,43 +2,36 @@
 #import "Fixtures.h"
 #import "ModuleFixtures.h"
 
-@interface RocketEngine : Engine <UnregisteredProtocol, Blinkable>
+@interface NewCarModule : JSObjectionModule
 @end
 
-@implementation RocketEngine
-@synthesize speed;
-@end
-
-@interface RocketScienceModule : JSObjectionModule
-@end
-
-@implementation RocketScienceModule
+@implementation NewCarModule
 -(void)configure {
-    [self bindClass:[RocketEngine class] toClass:[Engine class]];
+    [self bindClass:[UnregisteredCar class] toClass:[Car class]];
 }
 @end
 
-@interface ExperimentalCar : NSObject
-@property (nonatomic, retain) Engine<UnregisteredProtocol, Blinkable> *engine;
+@interface CarOwner : NSObject
+@property (nonatomic, strong) Car<UnregisteredProtocol> *car;
 @end
 
-@implementation ExperimentalCar
-objection_requires(@"engine")
+@implementation CarOwner
+objection_requires(@"car")
 @end
 
 QuickSpecBegin(InjectImplementsProtocolSpecs)
 
 __block JSObjectionInjector *injector = nil;
-__block ExperimentalCar *carWithRocketEngine = nil;
+__block CarOwner *carDriver = nil;
 
 beforeEach(^{
-    injector = [JSObjection createInjector:[RocketScienceModule new]];
-    carWithRocketEngine = [ExperimentalCar new];
+    injector = [JSObjection createInjector:[NewCarModule new]];
+    carDriver = [CarOwner new];
 });
 
-it(@"injects property engine that implements Blinkable and UnregisteredProtocol protocols ", ^{
-    [injector injectDependencies:carWithRocketEngine];
-    assertThat(carWithRocketEngine.engine, is(instanceOf([RocketEngine class])));
+it(@"injects property car that implements UnregisteredProtocol", ^{
+    [injector injectDependencies:carDriver];
+    assertThat(carDriver.car, is(instanceOf([UnregisteredCar class])));
 });
 
 QuickSpecEnd
